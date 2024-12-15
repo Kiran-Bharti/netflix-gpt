568bf031724a87c094f141913c7609b868cbee80
Kiran Bharti <kiranlko912@gmail.com>
2024-12-15 15:52:57 +0530

    Routing setup done

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>            
+        <div>
+            <Header />
+            <div>
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/729ce5c2-d831-436a-8c9d-f38fea0b99b3/web/SG-en-20241209-TRIFECTA-perspective_604f16f9-269e-410a-a034-9b4494d6280e_small.jpg" alt="netflix bg img" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
